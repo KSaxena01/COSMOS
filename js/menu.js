f8e7690f532634cbdda343a4280f5ea70b084203
KSaxena01 <63785306+KSaxena01@users.noreply.github.com>
2020-10-04 19:29:59 +0530

    Minor Change

--- js/menu.js
+++
@@ -1,6 +0,0 @@
-function slide(){
-	const burger = document.getElementsByClassName('burger');
-	const nav = document.getElementsByClassName('nav-links');
-	nav[0].classList.toggle('nav-active');
-	burger[0].classList.toggle('toggle');
-}
